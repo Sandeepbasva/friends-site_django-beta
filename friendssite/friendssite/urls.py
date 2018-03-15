52c6ba7951be41b5568c43454a32061c05d32ece
Sandeepbasva <sandeepbasva@gmail.com>
2018-03-15 21:56:33 +0530

    linked first page url in urls.py

--- friendssite/friendssite/urls.py
+++ friendssite/friendssite/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from myfriends.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^$', index),
+
 ]+static(settings.MEDIA_URL,
 document_root = settings.MEDIA_ROOT)
 
